800bbe31c6a507f6af8a570cc5da86918e684875
CopperPaint <jsaul1250@gmail.com>
2023-07-02 20:48:57 +0200

    try catch update

--- app/src/main/java/com/example/opsc_poe/home_category_view_fragment.kt
+++ app/src/main/java/com/example/opsc_poe/home_category_view_fragment.kt
@@ -18,7 +18,7 @@ class home_category_view_fragment : Fragment(R.layout.home_category_view_fragmen
     private var _binding: HomeCategoryViewFragmentBinding? = null
 
     // This property is only valid between onCreateView and
-// onDestroyView.
+    // onDestroyView.
     private val binding get() = _binding!!
 
     @SuppressLint("Range")
@@ -30,71 +30,68 @@ class home_category_view_fragment : Fragment(R.layout.home_category_view_fragmen
         _binding = HomeCategoryViewFragmentBinding.inflate(inflater, container, false)
         val view = binding.root
 
-
         //-------------------------------------------------
         //code here
-
-        var userHasData = false
-        for (i in GlobalClass.categories.indices) {
-
-            if (GlobalClass.categories[i].userID == GlobalClass.user.userID)
-            {
-                userHasData = true
-                break
-            }
-
-        }
-
-        if (userHasData == false)
-        {
-            GlobalClass.NoUserAppData(binding.llBars, activity, requireContext(), "Category", 0)
-        }
-        else
-        {
-
-            val activityLayout = binding.llBars
+        try {
+            var userHasData = false
+            //checks for user data
             for (i in GlobalClass.categories.indices)
             {
-                //if the category belongs to the signed in user
                 if (GlobalClass.categories[i].userID == GlobalClass.user.userID)
                 {
-
-                    //create new custom activity
-                    var newCategory = CustomActivity(activity)
-                    //get category data
-                    val catColour = ColorStateList.valueOf(Color.parseColor(GlobalClass.categories[i].colour))
-                    var (actTotal, hourTotal) = GetActivitesData(GlobalClass.categories[i].categoryID)
-                    //set primary text
-                    newCategory.binding.tvPrimaryText.text = GlobalClass.categories[i].name
-                    //set secondary text
-                    newCategory.binding.tvSecondaryText.text = "No of activties: " + actTotal
-                    //set the color of the divider bar between the text and the activity color shape
-                    newCategory.binding.vwBar.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.Default_Charcoal_Grey)
-                    //set the activity color shape color
-                    newCategory.binding.llBlockText.backgroundTintList = catColour
-                    //set the activity color block text
-                    newCategory.binding.tvBlockText.text = "Total Hours:"
-                    //set the activity color block time
-                    newCategory.binding.tvBlockX.text =  hourTotal.toString()
-                    newCategory.binding.tvBlockX.text = DoubleToTime(hourTotal.toString())
-
-                    newCategory.setOnClickListener(){
-                        var intent = Intent(activity, CategoryName::class.java)
-                        intent.putExtra("categoryIDIndex", i)
-                        startActivity(intent)
-                    }
-
-                    //add the new view
-                    activityLayout.addView(newCategory)
+                    userHasData = true
+                    break
                 }
             }
 
+            if (userHasData == false) //if user has no data
+            {
+                GlobalClass.NoUserAppData(binding.llBars, activity, requireContext(), "Category", 0)
+            }
+            else //if user has data
+            {
+                val activityLayout = binding.llBars
+                for (i in GlobalClass.categories.indices)
+                {
+                    //if the category belongs to the signed in user
+                    if (GlobalClass.categories[i].userID == GlobalClass.user.userID)
+                    {
+                        //create new custom activity
+                        var newCategory = CustomActivity(activity)
+                        //get category data
+                        val catColour = ColorStateList.valueOf(Color.parseColor(GlobalClass.categories[i].colour))
+                        var (actTotal, hourTotal) = GetActivitesData(GlobalClass.categories[i].categoryID)
+                        //set primary text
+                        newCategory.binding.tvPrimaryText.text = GlobalClass.categories[i].name
+                        //set secondary text
+                        newCategory.binding.tvSecondaryText.text = "No of activties: " + actTotal
+                        //set the color of the divider bar between the text and the activity color shape
+                        newCategory.binding.vwBar.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.Default_Charcoal_Grey)
+                        //set the activity color shape color
+                        newCategory.binding.llBlockText.backgroundTintList = catColour
+                        //set the activity color block text
+                        newCategory.binding.tvBlockText.text = "Total Hours:"
+                        //set the activity color block time
+                        newCategory.binding.tvBlockX.text =  hourTotal.toString()
+                        newCategory.binding.tvBlockX.text = DoubleToTime(hourTotal.toString())
+
+                        //category select binding
+                        newCategory.setOnClickListener(){
+                            var intent = Intent(activity, CategoryName::class.java)
+                            intent.putExtra("categoryIDIndex", i)
+                            startActivity(intent)
+                        }
+                        //add the new view
+                        activityLayout.addView(newCategory)
+                    }
+                }
+            }
         }
-
-
+        catch (e: Error)
+        {
+            GlobalClass.InformUser("Error", "${e.toString()}", requireContext())
+        }
         //-------------------------------------------------
-
-
         return view
     }
 
@@ -103,6 +100,7 @@ class home_category_view_fragment : Fragment(R.layout.home_category_view_fragmen
         _binding = null
     }
 
+    //method to get data from activities
     fun GetActivitesData(catID: Int): Pair<Int, Double>
     {
         var totalAct = 0
